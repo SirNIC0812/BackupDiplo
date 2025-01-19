fe5a2884a9fdaa7964053baaca3d832a013f47f0
Nico SCHAUENSTEIN <NSchauenst@bhakwien10.at>
2025-01-19 18:30:23 +0100

    Abteilung anzeigen?

--- Diplomarbeit_HHS/Models/AppUserService.cs
+++ Diplomarbeit_HHS/Models/AppUserService.cs
@@ -9,6 +9,8 @@ namespace Diplomarbeit_HHS.Models
 	public class AppUserService
 	{
 		private string _username = "xxx"; //string.Empty;
+		private string _rolle = "yyy"; //string.Empty;
+
 
 		public string Username
 		{
@@ -16,6 +18,15 @@ namespace Diplomarbeit_HHS.Models
 			set
 			{
 				_username = value;
+				NotifyStateChanged();
+			}
+		}
+		public string Rolle
+		{
+			get => _rolle;
+			set
+			{
+				_rolle = value;
 				NotifyStateChanged();
 			}
 		}
